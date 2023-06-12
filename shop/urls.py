47af9f31d0a058147c44aad29c847b5788a6c785
Slb59 <sylvie.bricout2@laposte.net>
2023-06-12 15:06:01 +0200

    product : ReadOnlyModelViewset

--- shop/urls.py
+++ shop/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 
 from rest_framework import routers
 
-from .views import CategoryViewset, ProductView
+from .views import CategoryViewset, ProductViewset
 
 app_name = 'shop'
 
@@ -11,8 +11,8 @@ router = routers.SimpleRouter()
 # Puis lui déclarons une url basée sur le mot clé ‘category’ et notre view
 # afin que l’url générée soit celle que nous souhaitons ‘/api/category/’
 router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('product/', ProductView.as_view())
+    path('', include(router.urls))
     ]
--- shop/views.py
+++ shop/views.py
@@ -1,18 +1,25 @@
-from rest_framework.views import APIView
+# from rest_framework.views import APIView
 from rest_framework.viewsets import ReadOnlyModelViewSet
-from rest_framework.response import Response
- 
+# from rest_framework.response import Response
+
 from .models import Category, Product
 from .serializers import CategorySerializer, ProductSerializer
- 
- 
+
+
 class CategoryViewset(ReadOnlyModelViewSet):
- 
+
     serializer_class = CategorySerializer
- 
+
     def get_queryset(self):
         return Category.objects.all()
-    
+
+
+class ProductViewset(ReadOnlyModelViewSet):
+
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
 
 # class CategoryView(APIView):
 
@@ -22,9 +29,9 @@ class CategoryViewset(ReadOnlyModelViewSet):
 #         return Response(serializer.data)
 
 
-class ProductView(APIView):
+# class ProductView(APIView):
 
-    def get(self, *args, **kwargs):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+#     def get(self, *args, **kwargs):
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
